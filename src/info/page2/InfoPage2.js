08279df62eeb956f41ffaddcecd1572098175419
Howard <howard504504@gmail.com>
2024-08-07 12:03:33 +0800

    早上總結

--- src/info/page2/InfoPage2.js
+++ src/info/page2/InfoPage2.js
@@ -1,310 +1,59 @@
 import React from "react";
 import "./InfoPage2.css";
+
 const InfoPage2 = () => {
 	return (
-		<div class="page" id="page2">
+		<div className="page" id="page2">
 			<video
-				autoplay
-				muted
+				autoPlay
+				muted={true}
 				loop
 				src="../大專素材/影片/狗/3045714-sd_640_360_25fps.mp4"
 			></video>
 			<h2>最新消息</h2>
-			<div class="currentCard">
+			<div className="currentCard">
 				<img
 					id="prev"
-					class="control"
+					className="control"
 					src="https://img.icons8.com/forma-light-filled/96/FFFFFF/circled-chevron-right.png"
 					alt="circled-chevron-right"
 				/>
-				<div class="cards">
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
-					<div class="card">
-						<img
-							id="cardImage"
-							src="../大專題模擬圖/1719287829-thumbnail.jpeg"
-						/>
-						<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
-						<span id="cardText">
-							📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
-							一起來逛台北寵物用品展!
-						</span>
-						<a href="#">more</a>
-					</div>
+				<div className="cards">
+					{Array.from({ length: 12 }).map((_, index) => (
+						<div className="card" key={index}>
+							<img
+								alt="最新資訊小卡"
+								id="cardImage"
+								src="../大專題模擬圖/1719287829-thumbnail.jpeg"
+							/>
+							<h3 id="cardTitle">【✨台北寵物用品展✨】</h3>
+							<span id="cardText">
+								📣台北寵物用品展來囉!! ​ 🐱🐶邀請毛孩爸、媽毛們
+								一起來逛台北寵物用品展!
+							</span>
+							<a href="http://localhost:3000/">more</a>
+						</div>
+					))}
 				</div>
 				<img
 					id="next"
-					class="control"
+					className="control"
 					src="https://img.icons8.com/forma-light-filled/96/FFFFFF/circled-chevron-right.png"
 					alt="circled-chevron-right"
 				/>
 			</div>
 			<div id="page2News">
-				<div id="newsInfo">
-					<p id="newsDate">2022.02.18</p>
-					<p id="newsText">
-						【慈愛海佃分院 停止營業公告】 ​ 親愛的顧客
-						感謝您一直以來對慈愛動物醫院 海佃分院的支持與厚愛 ​
-						因營運調整慈愛動物醫院海佃分院 ●最後服務日為 2024 年 2 月 27日
-						●停止營業日為 2024 年 2 月 28日 對於這造成的不便，我們深表歉意 ​。
-					</p>
-				</div>
-				<div id="newsInfo">
-					<p id="newsDate">2022.02.18</p>
-					<p id="newsText">
-						【慈愛海佃分院 停止營業公告】 ​ 親愛的顧客
-						感謝您一直以來對慈愛動物醫院 海佃分院的支持與厚愛 ​
-						因營運調整慈愛動物醫院海佃分院 ●最後服務日為 2024 年 2 月 27日
-						●停止營業日為 2024 年 2 月 28日 對於這造成的不便，我們深表歉意 ​。
-					</p>
-				</div>
-				<div id="newsInfo">
-					<p id="newsDate">2022.02.18</p>
-					<p id="newsText">
-						【慈愛海佃分院 停止營業公告】 ​ 親愛的顧客
-						感謝您一直以來對慈愛動物醫院 海佃分院的支持與厚愛 ​
-						因營運調整慈愛動物醫院海佃分院 ●最後服務日為 2024 年 2 月 27日
-						●停止營業日為 2024 年 2 月 28日 對於這造成的不便，我們深表歉意 ​。
-					</p>
-				</div>
-				<div id="newsInfo">
-					<p id="newsDate">2022.02.18</p>
-					<p id="newsText">
-						【慈愛海佃分院 停止營業公告】 ​ 親愛的顧客
-						感謝您一直以來對慈愛動物醫院 海佃分院的支持與厚愛 ​
-						因營運調整慈愛動物醫院海佃分院 ●最後服務日為 2024 年 2 月 27日
-						●停止營業日為 2024 年 2 月 28日 對於這造成的不便，我們深表歉意 ​。
-					</p>
-				</div>
-				<div id="newsInfo">
-					<p id="newsDate">2022.02.18</p>
-					<p id="newsText">
-						【慈愛海佃分院 停止營業公告】 ​ 親愛的顧客
-						感謝您一直以來對慈愛動物醫院 海佃分院的支持與厚愛 ​
-						因營運調整慈愛動物醫院海佃分院 ●最後服務日為 2024 年 2 月 27日
-						●停止營業日為 2024 年 2 月 28日 對於這造成的不便，我們深表歉意 ​。
-					</p>
-				</div>
-				<div id="newsInfo">
-					<p id="newsDate">2022.02.18</p>
-					<p id="newsText">
-						【慈愛海佃分院 停止營業公告】 ​ 親愛的顧客
-						感謝您一直以來對慈愛動物醫院 海佃分院的支持與厚愛 ​
-						因營運調整慈愛動物醫院海佃分院 ●最後服務日為 2024 年 2 月 27日
-						●停止營業日為 2024 年 2 月 28日 對於這造成的不便，我們深表歉意 ​。
-					</p>
-				</div>
-				<div id="newsInfo">
-					<p id="newsDate">2022.02.18</p>
-					<p id="newsText">
-						【慈愛海佃分院 停止營業公告】 ​ 親愛的顧客
-						感謝您一直以來對慈愛動物醫院 海佃分院的支持與厚愛 ​
-						因營運調整慈愛動物醫院海佃分院 ●最後服務日為 2024 年 2 月 27日
-						●停止營業日為 2024 年 2 月 28日 對於這造成的不便，我們深表歉意 ​。
-					</p>
-				</div>
-				<div id="newsInfo">
-					<p id="newsDate">2022.02.18</p>
-					<p id="newsText">
-						【慈愛海佃分院 停止營業公告】 ​ 親愛的顧客
-						感謝您一直以來對慈愛動物醫院 海佃分院的支持與厚愛 ​
-						因營運調整慈愛動物醫院海佃分院 ●最後服務日為 2024 年 2 月 27日
-						●停止營業日為 2024 年 2 月 28日 對於這造成的不便，我們深表歉意 ​。
-					</p>
-				</div>
-				<div id="newsInfo">
-					<p id="newsDate">2022.02.18</p>
-					<p id="newsText">
-						【慈愛海佃分院 停止營業公告】 ​ 親愛的顧客
-						感謝您一直以來對慈愛動物醫院 海佃分院的支持與厚愛 ​
-						因營運調整慈愛動物醫院海佃分院 ●最後服務日為 2024 年 2 月 27日
-						●停止營業日為 2024 年 2 月 28日 對於這造成的不便，我們深表歉意 ​。
-					</p>
-				</div>
-			</div>
-
-			<div class="footer" onclick="toggleFooter()">
-				<div class="footerIcon">
-					<img src="https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/96/FFFFFF/external-instagram-social-media-tanah-basah-basic-outline-tanah-basah.png" />
-					<img src="https://img.icons8.com/ios/100/FFFFFF/line-me.png" />
-					<img src="https://img.icons8.com/ios-filled/100/FFFFFF/twitterx--v2.png" />
-				</div>
-				<div id="buttonMoveUp">
-					<img src="https://img.icons8.com/ios-filled/100/FFFFFF/up--v1.png" />
-					<p>see more</p>
-					<span>地址:台中市公益路 二段 338號 8樓之8</span>
-					<span>連絡電話 : 04-2278-5847</span>
-					<span>隱私權政策</span>
-					<hr />
-					<span>Copy right©2024 Furry Health寵醫網</span>
-				</div>
+				{Array.from({ length: 10 }).map((_, index) => (
+					<div id="newsInfo" key={index}>
+						<p id="newsDate">2022.02.18</p>
+						<p id="newsText">
+							【慈愛海佃分院 停止營業公告】 ​ 親愛的顧客
+							感謝您一直以來對慈愛動物醫院 海佃分院的支持與厚愛 ​
+							因營運調整慈愛動物醫院海佃分院 ●最後服務日為 2024 年 2 月 27日
+							●停止營業日為 2024 年 2 月 28日 對於這造成的不便，我們深表歉意 ​。
+						</p>
+					</div>
+				))}
 			</div>
 		</div>
 	);
--- src/info/page3/InfoPage3.js
+++ src/info/page3/InfoPage3.js
@@ -2,16 +2,11 @@ import React from "react";
 import "./InfoPage3.css";
 
 const InfoPage3 = () => {
-	const toggleFooter = () => {
-		// 在這裡實現你的邏輯
-		console.log("Footer toggled");
-	};
-
 	return (
 		<div className="page" id="page3">
 			<video
 				autoPlay
-				muted
+				muted={true}
 				loop
 				src="../大專素材/影片/狗/3045714-sd_640_360_25fps.mp4"
 			></video>
@@ -22,6 +17,7 @@ const InfoPage3 = () => {
 			<div id="commonPage">
 				<div className="common">
 					<img
+						alt="home"
 						className="commonIcon"
 						src="https://img.icons8.com/ios-filled/100/FFFFFF/home.png"
 					/>
@@ -31,6 +27,7 @@ const InfoPage3 = () => {
 				</div>
 				<div className="common">
 					<img
+						alt="money"
 						className="commonIcon"
 						src="https://img.icons8.com/windows/96/FFFFFF/hand-holding-dollar.png"
 					/>
@@ -40,6 +37,7 @@ const InfoPage3 = () => {
 				</div>
 				<div className="common">
 					<img
+						alt="heavy"
 						className="commonIcon"
 						src="https://img.icons8.com/sf-regular/96/FFFFFF/like.png"
 					/>
@@ -49,31 +47,13 @@ const InfoPage3 = () => {
 				</div>
 				<div className="common">
 					<img
+						alt="howmany"
 						className="commonIcon"
 						src="https://img.icons8.com/ios/100/FFFFFF/dog--v1.png"
 					/>
 					<p className="commonText">正在受惠的貓狗總數</p>
 					<p className="commonNumber Number4">0</p>
 					<button className="commonButton">瞭解更多</button>
-				</div>
-			</div>
-			<div className="footer" onClick={toggleFooter}>
-				<div className="footerIcon">
-					<img src="https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/96/FFFFFF/external-instagram-social-media-tanah-basah-basic-outline-tanah-basah.png" />
-					<img src="https://img.icons8.com/ios/100/FFFFFF/line-me.png" />
-					<img src="https://img.icons8.com/ios-filled/100/FFFFFF/twitterx--v2.png" />
-				</div>
-				<div id="buttonMoveUp">
-					<img
-						alt="小狗"
-						src="https://img.icons8.com/ios-filled/100/FFFFFF/up--v1.png"
-					/>
-					<p>see more</p>
-					<span>地址:台中市公益路 二段 338號 8樓之8</span>
-					<span>連絡電話 : 04-2278-5847</span>
-					<span>隱私權政策</span>
-					<hr />
-					<span>Copy right©2024 Furry Health寵醫網</span>
 				</div>
 			</div>
 		</div>
